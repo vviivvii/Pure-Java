1479a685a2a50015270f93f086be72cd98aa5726
Sovattra <s.pen1@share.epsb.ca>
2019-11-25 11:49:30 -0700

    Create HelloWorld.java

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main (String[] args)
+  {
+    //System.out.println(" ");
+    System.out.println("/nHello World!/n");
+    //System.out.println(" ");
+  }
+}
