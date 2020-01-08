662cc88a9e827ad7ad9e6a0632c31e1a5bc2eab9
Sovattra <s.pen1@share.epsb.ca>
2020-01-08 10:51:58 -0700

    Update

--- Chatbot_v1/Chatbot_v1.java
+++ Chatbot_v1/Chatbot_v1.java
@@ -10,7 +10,7 @@ public static void main(String[] args)
 {
 	Scanner in = new Scanner ( System.in );
 	System.out.println("\nHello my name is Chatbot"+"\nLet's Talk");
-	
+
 	int i=0;
 	double x=0;
 	Boolean endChatbot = false;
@@ -26,12 +26,18 @@ public static void main(String[] args)
 		} else {
 			input = in.nextLine();
 			response[i] = "\nYou: " + input;
-			 x = ((Math.random() * 2) + 1);
-			 if (x >= 2) {
+			 x = ((Math.random() * 4) + 1);
+			 if (x >= 2 && x <= 3 ) {
 				 statement[i] = "Chatbot: " + "Very nice!\n";
-				 //System.out.println(x);
-			 }	else if (x <= 2 ){
+				//System.out.println(x);
+			 }	else if (x <= 2){
 				 statement[i] = "Chatbot: " + "Cool tell me more.\n";
+				 //System.out.println(x);
+			 }else if (x >= 3 && x <= 4){
+				 statement[i] = "Chatbot: " + "You don't say.\n";
+				 //System.out.println(x);
+			 }else if (x >= 4){
+				 statement[i] = "Chatbot: " + "Interesting, tell me more.\n";
 				 //System.out.println(x);
 			 }
 			System.out.println(response[i]);
