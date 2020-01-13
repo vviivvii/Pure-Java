3bee28ab8053bf0d1b0f59e28ddfc05a1673a1bc
Sovattra <s.pen1@share.epsb.ca>
2020-01-13 11:44:49 -0700

    Update

--- Chatbot_v1/Chatbot_v1.java
+++ Chatbot_v1/Chatbot_v1.java
@@ -12,28 +12,35 @@ public static void main(String[] args)
 	System.out.println("<System> " + "See Dictionary of Responses to talk to Chatbot");
 	System.out.println("\nChatbot: " + "Hello my name is Chatbot"+ "\nChatbot: " + "Let's Talk\n");
 
+//Variables
 	int i=0;
 	double x=0;
 	Boolean endChatbot = false;
 	String input;
-	//Types of Chatbot responses
+	int nothing = 0;
+
+//What the user can say to the Chatbot
 	String no = "no";
+	String yes = "yes";
 	String chatbot = "chatbot";
 	String hello = "hello";
 	String mother = "mother";
 	String father = "father";
 	String sister = "sister";
 	String brother = "brother";
+	String noResponse = "";
+
+//Chatbot code
 	while ( endChatbot == false ) {
 		if ( i >= response.length ) {
 			endChatbot = true;
 		}else if (i == 48) {
-			statement[i] = "Chatbot: " + "We are about to run out of memory." +
-											"\nChatbot: GoodBye.\n";
+			statement[i] = "Chatbot: " + "We are about to run out of memory." + "\nChatbot: GoodBye.\n";
 			System.out.println(statement[i]);
 		} else {
 			input = in.nextLine();
 			response[i] = "\nYou: " + input;
+			//Chatbot's Random Responses.
 			 x = ((Math.random() * 4) + 1);
 			  if (x >= 2 && x <= 3 ) {
 				 statement[i] = "Chatbot: " + "Very nice!\n";
@@ -43,24 +50,36 @@ public static void main(String[] args)
 				 statement[i] = "Chatbot: " + "You don't say.\n";
 			 } else if (x >= 4){
 				 statement[i] = "Chatbot: " + "Interesting, tell me more.\n";
+			 }
 				 /*
-				 || Chatbot responses ||
+				 --|| Chatbot responses ||--
 				*/
-			} if (input.equals(no) == true ) {
+				if (nothing >= 20) { // If the user said nothing in 20 lines pr more the chatbot would ask the user to say something.
+ 				  statement[i] = "Chatbot: " + "Can you say soemthing?\n";
+ 			 } if (input.equals(no) == true ) {
 				 statement[i] = "Chatbot: " + "That was mean of you.\n";
-			 } if (input.equals(chatbot) == true ) {
+			 } if (input.equals(yes) == true ) {
+ 				 statement[i] = "Chatbot: " + "I agree with you too\n";
+ 			 } if (input.equals(chatbot) == true ) {
  				 statement[i] = "Chatbot: " + "That's my name.\n";
- 			 }
-			 if (input.equals(hello) == true ) {
+ 			 }if (input.equals(hello) == true ) {
  				 statement[i] = "Chatbot: " + "Hello user.\n";
- 			 }
-			 if (input.equals(mother) == true || input.equals(father) == true || input.equals(sister) == true || input.equals(brother) == true) {
+ 			 }if (input.equals(mother) == true || input.equals(father) == true || input.equals(sister) == true || input.equals(brother) == true) {
  				 statement[i] = "Chatbot: " + "Tell me more about your family.\n";
- 			 }
+
+
+				//If the user is not talking (User keeps on pressing/holding enter) the chatbot would ask the user to say something.
+ 			 }if (input.equals(noResponse) == true ) {
+				 nothing++;
+			 }else { // The counter would go down by two if the user says something.
+				 nothing--;
+				 nothing--;
+			 }
+			//To show the user (response) and chatbot (statement) talking.
 			System.out.println(response[i]);
 			System.out.println(statement[i]);
 		}
 		i++;
   }
-}
+ }
 }
